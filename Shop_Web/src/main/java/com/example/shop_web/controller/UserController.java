da67487ee4bb1b58dc9b8e4b7b411231345f945b
vietlongbkdp <vietlongbkdp@gmail.com>
2023-11-21 08:07:54 +0700

    no message

--- Shop_Web/src/main/java/com/example/shop_web/controller/UserController.java
+++ Shop_Web/src/main/java/com/example/shop_web/controller/UserController.java
@@ -1,4 +1,13 @@
 package com.example.shop_web.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+@RequestMapping(value = "/home")
 public class UserController {
+    @GetMapping
+    public String show(){
+        return "admin/createProduct";
+    }
 }
