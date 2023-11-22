c7e4aab3163b67a8dce771d0834cf348fc37425c
QuocHuy248 <quochuy248@gmail.com>
2023-11-22 11:29:12 +0700

    no message

--- Shop_Web/src/main/java/com/example/shop_web/security/config/SpringSecurity.java
+++ Shop_Web/src/main/java/com/example/shop_web/security/config/SpringSecurity.java
@@ -28,7 +28,7 @@ public class SpringSecurity {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeHttpRequests((authorize) ->
-                                authorize.requestMatchers("/register/**", "/test").permitAll()
+                                authorize.requestMatchers("/register/**", "/test","/products").permitAll()
                                         .requestMatchers("/home","/booking","/services","/portfolio").permitAll()
                                         .requestMatchers("/admin").hasRole("ADMIN")
 //                                .requestMatchers("/stylist").hasRole("ADMIN")
