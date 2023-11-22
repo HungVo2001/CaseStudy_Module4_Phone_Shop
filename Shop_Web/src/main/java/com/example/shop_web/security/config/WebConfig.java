c2753a55d93aed639c295c639b22c18f4037eebb
vietlongbkdp <vietlongbkdp@gmail.com>
2023-11-22 11:28:01 +0700

    update homePage

--- Shop_Web/src/main/java/com/example/shop_web/security/config/WebConfig.java
+++ Shop_Web/src/main/java/com/example/shop_web/security/config/WebConfig.java
@@ -1,15 +1,15 @@
-package com.example.shop_web.security.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
-
-@Configuration
-public class WebConfig {
-
-    @Bean
-    public JavaMailSender getJavaMailSender () {
-        return new JavaMailSenderImpl();
-    }
-}
+//package com.example.shop_web.security.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.mail.javamail.JavaMailSender;
+//import org.springframework.mail.javamail.JavaMailSenderImpl;
+//
+//@Configuration
+//public class WebConfig {
+//
+//    @Bean
+//    public JavaMailSender getJavaMailSender () {
+//        return new JavaMailSenderImpl();
+//    }
+//}
