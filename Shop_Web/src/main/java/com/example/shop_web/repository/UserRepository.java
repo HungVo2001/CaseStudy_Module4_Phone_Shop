b5e915cf888b8f3492b0fed2452c283b77d89c5b
QuocHuy248 <quochuy248@gmail.com>
2023-11-24 09:19:46 +0700

    error

--- Shop_Web/src/main/java/com/example/shop_web/repository/UserRepository.java
+++ Shop_Web/src/main/java/com/example/shop_web/repository/UserRepository.java
@@ -1,19 +1,19 @@
-package com.example.shop_web.repository;
-
-import com.example.shop_web.domain.Branch;
-import com.example.shop_web.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
-
-@Repository
-public interface UserRepository extends JpaRepository<User,Long> {
-
-
-    Boolean existsByUsername(String userName);
-
-    Optional<User> findByUsername(String userName);
-
-    User getByUsername(String userName);
-}
+//package com.example.shop_web.repository;
+//
+//import com.example.shop_web.domain.Branch;
+//import com.example.shop_web.domain.User;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//
+//import java.util.Optional;
+//
+//@Repository
+//public interface UserRepository extends JpaRepository<User,Long> {
+//
+//
+//    Boolean existsByUsername(String userName);
+//
+//    Optional<User> findByUsername(String userName);
+//
+//    User getByUsername(String userName);
+//}
